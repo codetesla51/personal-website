b38bcbbcb60c5391f7936d56c5a81445578a19bb
codetesla <uoladele99@gmail.com>
2024-11-15 16:57:19 +0100

    Modified existing project files.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,10 @@ import Home from '$lib/components/sections/home.svelte';
 import Stack from '$lib/components/sections/stack.svelte';
 import Project from '$lib/components/sections/projects.svelte';
 import Overlay from '$lib/components/reusable/overlay.svelte';  
-
+import Contact from '$lib/components/sections/contact.svelte';
 </script> 
   <Home />
   <Stack />
 <Project />
+<Contact />
 <Overlay />
