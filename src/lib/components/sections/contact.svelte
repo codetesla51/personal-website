99d0dc28a976bda29d7d1a2681d79834756414d7
codetesla <uoladele99@gmail.com>
2024-11-15 16:56:38 +0100

    Added new project files.

---
+++ src/lib/components/sections/contact.svelte
@@ -0,0 +1,17 @@
+<script>
+  
+</script>
+<section>
+  <div class="grid grid-cols-3">
+    <div class="flex justify-between items-center w-full rounded">
+  <div class="flex items-center justify-center">
+    <i class="fab fa-facebook"></i>
+    <h3>@UthmanDev</h3>
+    </div>
+    <div>
+      <i class="fa fa-arrow-up"></i>
+    </div>
+    </div>
+    
+  </div>
+</section>
